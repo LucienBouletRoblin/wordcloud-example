536238d96ad426992137bcaad64345622d8c86f2
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2019-02-19 07:56:38 +0100

    initial commit wordcloud

---
+++ build/precache-manifest.4371f5834cece90f2fee07a96f2b9074.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "27ac53edeac1ddbcec30",
+    "url": "/static/js/main.27ac53ed.chunk.js"
+  },
+  {
+    "revision": "8df3fa0f71a2e8a3bc6a",
+    "url": "/static/js/2.8df3fa0f.chunk.js"
+  },
+  {
+    "revision": "27ac53edeac1ddbcec30",
+    "url": "/static/css/main.d1942b5f.chunk.css"
+  },
+  {
+    "revision": "e14707cd421f8c0d7401c1ee007fb4ed",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import ReactWordCloud from "react-wordcloud";
+import { Grid } from "@material-ui/core";
+
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 200
+  }
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      words: [
+        { word: "hello", value: 3 },
+        { word: "world", value: 1 },
+        { word: "github", value: 1 },
+        { word: "code", value: 1 }
+      ]
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ text: event.target.value });
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Grid container justify="center" direction="row">
+        <Grid item xs={12}>
+          <form className={classes.container} noValidate autoComplete="off">
+            <TextField
+              id="outlined-full-width"
+              label="Mots à afficher dans le nuage de mots"
+              multiline
+              style={{ margin: 8 }}
+              placeholder="Placeholder"
+              helperText="Le texte ou les mots seront analysés, puis leur occurence sera proportionnelle à la taille d'écriture du mot dans le nuage ci-dessous :"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true
+              }}
+              value={this.state.text}
+              onChange={this.handleChange}
+            />
+          </form>
+        </Grid>
+        <Grid item xs={12}>
+          <div style={{ width: 600, height: 400 }}>
+            <ReactWordCloud
+              words={this.state.words}
+              wordCountKey={"value"}
+              wordKey={"word"}
+            />
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+export default withStyles(styles)(App);
